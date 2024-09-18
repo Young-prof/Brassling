5254b8826cf6b3e694d40f23ae588d86541b8ea7
Young-prof <frankiperezi99@gmail.com>
2024-09-18 08:39:33 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+        // Toggle the mobile menu
+        document.getElementById('mobile-menu').addEventListener('click', function() {
+            var navLinks = document.getElementById('nav-links');
+            if (navLinks.style.display === 'block') {
+                navLinks.style.display = 'none';
+            } else {
+                navLinks.style.display = 'block';
+            }
+        });
